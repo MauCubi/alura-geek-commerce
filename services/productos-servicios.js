d4f13a2caf769a87b43fd9c657592b286eb30a09
Mauro <dantuchiii@gmail.com>
2023-01-19 18:33:41 -0300

    fix routes

--- services/productos-servicios.js
+++ services/productos-servicios.js
@@ -1,7 +1,8 @@
 // GET
 
 // Toda la respuesta la almacenamos en un json
-const listaProductos = () => fetch("http://localhost:3000/producto").then(respuesta => respuesta.json()); 
+// const listaProductos = () => fetch("http://localhost:3000/producto").then(respuesta => respuesta.json()); 
+const listaProductos = () => fetch(" https://my-json-server.typicode.com/producto").then(respuesta => respuesta.json());
 
 const listaBusqueda = (name) => fetch(`http://localhost:3000/producto?name_like=${name}`).then(respuesta => respuesta.json()); 
 
