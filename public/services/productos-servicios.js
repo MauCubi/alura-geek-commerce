b1504d6f420ce7d234e89d245d4941cab60dbd89
Mauro <dantuchiii@gmail.com>
2023-01-23 17:13:51 -0300

    Fetch url change

--- public/services/productos-servicios.js
+++ public/services/productos-servicios.js
@@ -3,14 +3,14 @@
 // Toda la respuesta la almacenamos en un json
 const listaProductos = () => fetch(`https://${window.location.hostname}/producto`).then(respuesta => respuesta.json()); 
 
-const listaBusqueda = (name) => fetch(`https://alura-geek.onrender.com/producto?name_like=${name}`).then(respuesta => respuesta.json()); 
+const listaBusqueda = (name) => fetch(`https://${window.location.hostname}/producto?name_like=${name}`).then(respuesta => respuesta.json()); 
 
-const listaSimilares = (section) => fetch(`https://alura-geek.onrender.com/producto?section=${section}`).then(respuesta => respuesta.json()); 
+const listaSimilares = (section) => fetch(`https://${window.location.hostname}/producto?section=${section}`).then(respuesta => respuesta.json()); 
 
 
 const crearProducto = (name, imageUrl, price, section, descripcion) => {
   console.log("EH");
-    return fetch('https://alura-geek.onrender.com/producto', {
+    return fetch(`https://${window.location.hostname}/producto`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json'
@@ -20,11 +20,11 @@ const crearProducto = (name, imageUrl, price, section, descripcion) => {
   }
 
   const detalleProducto = (id) => {
-    return fetch(`https://alura-geek.onrender.com/producto/${id}`).then( (respuesta) =>  respuesta.json() );
+    return fetch(`https://${window.location.hostname}/producto/${id}`).then( (respuesta) =>  respuesta.json() );
   }
 
   const actualizarProducto = (name, price, imageUrl, section, descripcion, id) => {
-    return fetch(`https://alura-geek.onrender.com/producto/${id}`, {
+    return fetch(`https://${window.location.hostname}/producto/${id}`, {
       method: 'PUT',
       headers: {
         'content-type': 'application/json'
@@ -36,7 +36,7 @@ const crearProducto = (name, imageUrl, price, section, descripcion) => {
   }
 
 const eliminarProducto = (id) => {
-  return fetch(`https://alura-geek.onrender.com/producto/${id}`, {
+  return fetch(`https://${window.location.hostname}/producto/${id}`, {
     method: 'DELETE'    
   })
 }
